7634098936440639acd14e36bd630e6e8a8e7750
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-24 14:10:32 +0530

    Update __main__.py

--- miley/__main__.py
+++ miley/__main__.py
@@ -44,13 +44,13 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
-🔥 - /start: start the bot
-🔥 - /help: PM's you this message.
-🔥 - /help <module name>: PM's you info about that module.
-🔥 - /source: Information about my source.
-🔥 - /settings:
-   🔹 - in PM: will send you your settings for all supported modules.
-   🔹 - in a group: will redirect you to pm, with all that chat's settings.
+ ★  - /start: 𝓢𝓽𝓪𝓻𝓽 𝓽𝓱𝓮 𝓫𝓸𝓽
+ ★  - /help: PM's you this message.
+ ★  - /help <module name>: PM's you info about that module.
+ ★  - /source: Information about my source.
+ ★  - /settings:
+   ✪ - in PM: will send you your settings for all supported modules.
+  ✪  - in a group: will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
